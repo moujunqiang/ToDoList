7d09568d172f68a2273c74b015dc43d0bf2b81cc
yyyyyyyj14 <17810255081@163.com>
2020-12-06 21:42:53 +0800

    Initial commit

---
+++ app/src/main/java/hk/polyu/eie/eie3109/todolist/SalesDB.java
@@ -0,0 +1,35 @@
+package hk.polyu.eie.eie3109.todolist;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class SalesDB extends SQLiteOpenHelper {
+    public static final String DATABASE_NAME="SalesDB";
+    public static final int DATABASE_VERSION=1;
+    public static final String TABLE_CUSTOMER="customer";
+    public static final String CUSTOMER_ID="id";
+    public static final String CUSTOMER_NAME="name";
+    public static final String CUSTOMER_GENDER="gender";
+
+    public SalesDB(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, DATABASE_NAME, factory, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String sql="CREATE TABLE "+TABLE_CUSTOMER+"("
+                +CUSTOMER_ID+"INTEGER primary key autoincrement,"
+                +CUSTOMER_NAME+"text,"
+                +CUSTOMER_GENDER+"text)";
+        db.execSQL(sql);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
